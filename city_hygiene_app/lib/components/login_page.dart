c950ad742e1fc994bb2267487f7556d6af404cb9
kaffa <kaffa.askia@intouchgroup.net>
2024-02-18 18:00:36 +0000

    Initial commit

---
+++ city_hygiene_app/lib/components/login_page.dart
@@ -0,0 +1,134 @@
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:developer';
+
+import 'package:city_hygiene_app/components/common/custom_form_button.dart';
+import 'package:city_hygiene_app/components/common/custom_input_field.dart';
+import 'package:city_hygiene_app/components/common/page_header.dart';
+import 'package:city_hygiene_app/components/common/page_heading.dart';
+import 'package:city_hygiene_app/components/signup_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _loginFormKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color(0xffEEF1F3),
+        body: Column(
+          children: [
+            const PageHeader(),
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(20),
+                  ),
+                ),
+                child: SingleChildScrollView(
+                  child: Form(
+                    key: _loginFormKey,
+                    child: Column(
+                      children: [
+                        const PageHeading(
+                          title: 'Log-in',
+                        ),
+                        CustomInputField(
+                            labelText: 'Username',
+                            hintText: 'Your username',
+                            validator: (textValue) {
+                              if (textValue == null || textValue.isEmpty) {
+                                return 'Username is required!';
+                              }
+                              return null;
+                            }),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        CustomInputField(
+                          labelText: 'Password',
+                          hintText: 'Your password',
+                          obscureText: true,
+                          suffixIcon: true,
+                          validator: (textValue) {
+                            if (textValue == null || textValue.isEmpty) {
+                              return 'Password is required!';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        CustomFormButton(
+                          innerText: 'Login',
+                          onPressed: _handleLoginUser,
+                        ),
+                        const SizedBox(
+                          height: 18,
+                        ),
+                        SizedBox(
+                          width: size.width * 0.8,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                'Don\'t have an account ? ',
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    color: Color(0xff939393),
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              GestureDetector(
+                                onTap: () => {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const SignupPage()))
+                                },
+                                child: const Text(
+                                  'Sign-up',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      color: Color(0xff748288),
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _handleLoginUser() {
+    // login user
+    if (_loginFormKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Submitting data..')),
+      );
+    }
+  }
+}
